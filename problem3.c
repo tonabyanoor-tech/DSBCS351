0510699079249c6f4a377918a9538f61715d6ceb
tonabyanoor-tech <tonabyanoor@gmail.com>
2026-08-20 23:17:29 +0530

    Add files via upload

---
+++ problem3.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+void traverse(struct Node *head) {
+    struct Node *temp = head;
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+}
+
+int main() {
+    struct Node *head = malloc(sizeof(struct Node));
+    struct Node *second = malloc(sizeof(struct Node));
+    struct Node *third = malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = NULL;
+
+    printf("Linked List: ");
+    traverse(head);
+
+    free(third);
+    free(second);
+    free(head);
+
+    return 0;
+}
